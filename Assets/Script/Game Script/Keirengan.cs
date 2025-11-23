3cc8b2587dddd14caa507329351d9b5c3d869f7e
Markus Christiano Simanjuntak <111673156+ThePassive@users.noreply.github.com>
2025-11-23 23:48:23 +0700

    New BG, Layering, bug fixes
    + New BG
    +Set some layers
    - Bug: monke wont throw anything

--- Assets/Script/Game Script/Keirengan.cs
+++ Assets/Script/Game Script/Keirengan.cs
@@ -9,13 +9,13 @@ public class Keirengan : MonoBehaviour
 
     [Header("Time Settings")]
     [Tooltip("Lama dia diam di kondisi TERANG (Transparan)")]
-    public float intervalTerang = 5f;
+    public float intervalTerang = 10f;
 
     [Tooltip("Berapa lama dia diam di kondisi GELAP (Hitam)")]
-    public float durasiGelap = 1f;
+    public float durasiGelap = 10f;
 
     [Tooltip("Berapa lama proses berubahnya?")]
-    public float durasiTransformasi = 2f;
+    public float durasiTransformasi = 5f;
 
     [Header("Darkness Setting")]
     [Range(0f, 1f)]
